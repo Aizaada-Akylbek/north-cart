597875fe8d21acefe37e1eec52448a6c0e9007ac
Aizaada-Akylbek <88928940+Aizaada-Akylbek@users.noreply.github.com>
2024-08-03 15:38:30 +0600

    created

---
+++ src/components/layout/Layout.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import Header from "../hedaer/Header";
+import { Route, Routes } from "react-router-dom";
+import Home from "../../pages/Home";
+import About from "../../pages/About";
+import Contact from "../../pages/Contact";
+import Detail from "../../pages/Detail";
+import Cart from "../../pages/Cart";
+
+const Layout = () => {
+  return (
+    <div>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/detail" element={<Detail />} />
+        <Route path="/cart" element={<Cart />} />
+      </Routes>
+    </div>
+  );
+};
+
+export default Layout;
